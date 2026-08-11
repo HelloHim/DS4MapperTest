3198b82209253040fc7910cf5ced3c9d8374d4b5
HelloHim <121320973+HelloHim@users.noreply.github.com>
2026-08-11 13:39:24 +0100

    refactor(ui): fold stick mode bindings

--- DS4MapperTest/Views/StickSideModeSettingsControl.xaml.cs
+++ DS4MapperTest/Views/StickSideModeSettingsControl.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using DS4MapperTest.ViewModels;
 
 namespace DS4MapperTest.Views
 {
@@ -7,6 +9,51 @@ namespace DS4MapperTest.Views
         public StickSideModeSettingsControl()
         {
             InitializeComponent();
+        }
+
+        private void ExtraAddExtraBindingButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.ContextMenu != null)
+            {
+                button.ContextMenu.PlacementTarget = button;
+                button.ContextMenu.IsOpen = true;
+            }
+        }
+
+        private void ExtraAddExtraBindingMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not MenuItem menuItem ||
+                menuItem.Tag is not string tag ||
+                menuItem.Parent is not ContextMenu menu ||
+                menu.PlacementTarget is not FrameworkElement target ||
+                target.DataContext is not StickExtraBindingItem bindingItem)
+            {
+                return;
+            }
+
+            FaceBindingFuncKind? kind = tag switch
+            {
+                "Hold" => FaceBindingFuncKind.Hold,
+                "Double" => FaceBindingFuncKind.Double,
+                "Distance" => FaceBindingFuncKind.Distance,
+                "Chorded" => FaceBindingFuncKind.Chorded,
+                "SimPress" => FaceBindingFuncKind.SimPress,
+                "Start" => FaceBindingFuncKind.Start,
+                "Release" => FaceBindingFuncKind.Release,
+                _ => null,
+            };
+
+            if (kind == null) return;
+
+            bindingItem.AddExtraBinding(kind.Value);
+        }
+
+        private void ExtraRemoveBinding_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button { Tag: StickExtraFuncItem item })
+            {
+                item.Owner.RemoveBinding(item);
+            }
         }
     }
 }
