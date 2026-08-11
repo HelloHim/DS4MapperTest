0d4aef781947d24dade8b3e4fde09248edb5f1b3
HelloHim <121320973+HelloHim@users.noreply.github.com>
2026-08-11 13:39:24 +0100

    refactor(ui): fold stick mode bindings

--- DS4MapperTest/Views/StickSideBindingsControl.xaml.cs
+++ DS4MapperTest/Views/StickSideBindingsControl.xaml.cs
@@ -55,50 +55,5 @@ namespace DS4MapperTest.Views
                 item.Owner.RemoveBinding(item);
             }
         }
-
-        private void ExtraAddExtraBindingButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button && button.ContextMenu != null)
-            {
-                button.ContextMenu.PlacementTarget = button;
-                button.ContextMenu.IsOpen = true;
-            }
-        }
-
-        private void ExtraAddExtraBindingMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is not MenuItem menuItem ||
-                menuItem.Tag is not string tag ||
-                menuItem.Parent is not ContextMenu menu ||
-                menu.PlacementTarget is not FrameworkElement target ||
-                target.DataContext is not StickExtraBindingItem bindingItem)
-            {
-                return;
-            }
-
-            FaceBindingFuncKind? kind = tag switch
-            {
-                "Hold" => FaceBindingFuncKind.Hold,
-                "Double" => FaceBindingFuncKind.Double,
-                "Distance" => FaceBindingFuncKind.Distance,
-                "Chorded" => FaceBindingFuncKind.Chorded,
-                "SimPress" => FaceBindingFuncKind.SimPress,
-                "Start" => FaceBindingFuncKind.Start,
-                "Release" => FaceBindingFuncKind.Release,
-                _ => null,
-            };
-
-            if (kind == null) return;
-
-            bindingItem.AddExtraBinding(kind.Value);
-        }
-
-        private void ExtraRemoveBinding_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button { Tag: StickExtraFuncItem item })
-            {
-                item.Owner.RemoveBinding(item);
-            }
-        }
     }
 }
